1ad9d76a89040f036bbdfbdae8bdbf2cee678b69
Jonas Nielsen <djonnneller@gmail.com>
2024-09-25 16:22:04 +0200

    Scrape more detailed job descriptions

--- parse.py
+++ parse.py
@@ -1,15 +1,26 @@
 from bs4 import BeautifulSoup
 from helpers import remove_blank_lines
+from urllib.parse import urlparse, parse_qs
 
-def parse(content, filewriter):
+def parse_archive_description(content):
+    try:
+        soup = BeautifulSoup(content, "html.parser")
+        job_postings = soup.find(class_="jobtext-jobad__body")
+        return remove_blank_lines(job_postings.get_text())
+    except:
+        return None
+
+def parse(content, callback):
     soup = BeautifulSoup(content, "html.parser")
-    job_postings_paid = soup.find_all("div", class_="jobsearch-result")
+    job_postings = soup.find_all("div", class_="jobsearch-result")
 
-    print(f"Found {len(job_postings_paid)} postings")
+    print(f"Found {len(job_postings)} postings")
 
-    for _, job in enumerate(job_postings_paid):
+    for _, job in enumerate(job_postings):
 
         try:
+            if(not is_original_posting(job)): continue
+
             company = get_company(job)
             title = get_title(job)
             location = get_location(job)
@@ -17,11 +28,11 @@ def parse(content, filewriter):
             timestamp = get_timestamp(job)
             archive_link = get_archive_link(job)
 
-            filewriter({
+            callback({
                     "company": company,
                     "title": title,
                     "location": location,
-                    "description": description,
+                    "short_description": description,
                     "timestamp": timestamp,
                     "archive_link": archive_link,
                 })
@@ -29,7 +40,6 @@ def parse(content, filewriter):
             print(job)
             raise RuntimeError("stuff broke")
 
-    # return True ## DELETE ME
     return is_last_page(soup)
 
 
@@ -37,6 +47,11 @@ def is_last_page(page):
     current_page = page.find("ul", class_="pagination").find_all("li", class_="active", limit=1)[0].get_text().strip()
     last_page = page.find("ul", class_="pagination").find_all(class_="page-link")[-1].get_text().strip()
     return current_page == last_page
+
+def is_original_posting(job):
+    href = job.find("h4").find("a")["href"]
+    hostname = urlparse(href).hostname
+    return hostname == "www.jobindex.dk"
 
 def get_company(job):
     company = job.find(class_="jix-toolbar-top__company")
@@ -65,4 +80,7 @@ def get_timestamp(job):
     return remove_blank_lines(job.find(class_="jix-toolbar__pubdate").find("time").get_text())
 
 def get_archive_link(job):
-    return "https://www.jobindex.dk" + job.find("a", class_="seejobdesktop")["data-click"]
+    url = "https://www.jobindex.dk" + job.find("a", class_="seejobdesktop")["data-click"]
+    query = urlparse(url).query
+    id = parse_qs(query)["t"][0]
+    return "https://www.jobindexarkiv.dk/cgi/showarchive.cgi?rm=content&tid="+id
--- scrape.py
+++ scrape.py
@@ -1,9 +1,10 @@
 import requests
 import json
-from parse import parse
+from parse import parse, parse_archive_description
 
-page_number_limit = 5000
+page_number_limit = 100
 file = open("output/output.json", "w")
+job_listings = []
 
 def build_url(min_date: str, max_date: str, page_number: int, categories: list[int]):
     subid = ("").join(f"&subid={x}" for x in categories) if categories != None else ""
@@ -20,50 +21,58 @@ def build_url(min_date: str, max_date: str, page_number: int, categories: list[i
     )
 
 def write_to_file(datapoint):
-    file.write(json.dumps(datapoint))
-    file.write(",\n")
+    file.write(json.dumps(datapoint, indent=2))
 
+def remember(datapoint):
+    job_listings.append(datapoint)
 
-def scrape(max_date, min_date, page_number, categories):
+def scrape_search(max_date, min_date, page_number, categories):
     url = build_url(min_date, max_date, page_number, categories)
 
     print("fetching data from url: {0}".format(url))
     page = requests.get(url)
 
     if len(page.content) > 0:
-        return parse(page.content, write_to_file)
+        return parse(page.content, remember)
 
     return True
 
+def scrape_archived_listing(listing):
+    url = listing["archive_link"]
 
-def format_date(date):
-    return ("").join((date).split("-"))
+    print("fetching data from url: {0}".format(url))
+    page = requests.get(url)
 
+    if len(page.content) > 0:
+        listing["description"] = parse_archive_description(page.content)
 
-def url_format_date(date):
-    return ("").join(date.split("-"))
+    return listing
 
 
 def run(page_number, min_date, max_date, categories):
     while page_number <= page_number_limit:
 
-        is_done = scrape(
+        is_done = scrape_search(
             page_number=page_number, max_date=max_date, min_date=min_date, categories=categories
         )
 
         if is_done:
             print("DONE!")
-            return
+            break
         
         page_number += 1
 
+    print(f"Found {len(job_listings)} from search")
+    for listing in job_listings:
+        scrape_archived_listing(listing)
+
+    write_to_file(job_listings)
+
 
 if __name__ == '__main__':
-    file.write("[\n")
     run(
         page_number=1,
         min_date="20240801",
         max_date="20240901",
         categories=[11,8,85] # Bygge- og anlægsteknik, Elektroteknik, Maskinteknik
     )
-    file.write("]")
