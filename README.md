0d2232f08b6384c7a73c62f44029ca7b63dd85ae
Jonas Nielsen <djonnneller@gmail.com>
2024-09-30 22:23:09 +0200

    Add adjective analyzer

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 Scrapes [Jobindex](https://www.jobindex.dk/) using python and BeautifulSoup.
 Based on work from [Pedro Madruga](https://github.com/pmadruga/jobindex-scraper).
 
+Additionally counts occurences of adjectives in the found job listings.
+Uses ["Det Centrale Ordregister" COR (en: The Central Word Register)](https://ordregister.dk/) for a list of adjectives.
+
 ## Setup (linux)
 
 - `python -m venv .venv`
@@ -13,4 +16,5 @@ Based on work from [Pedro Madruga](https://github.com/pmadruga/jobindex-scraper)
 
 - Set time interval, categories and areas in "scraper/scrape.py"
 - `source .venv/bin/activate` (if not done already)
-- `python scraper/scrape.py`
+- `python scraper/scrape.py` to start scraping jobindex
+- `python analysis/analyzer.py` to find adjectives and count them by listing category
---
+++ analysis/analyzer.py
@@ -0,0 +1,95 @@
+import os
+import json
+import re
+
+path = os.path.dirname(os.path.abspath(__file__))
+wordlist_path = path + "/cor1.02.tsv"
+categories_path = path + "/category_to_subid_mapping.json"
+listings_path = path + "/../output" + "/scraped_data.json"
+result_path = path + "/../output" + "/adjective_counts_by_category.json"
+
+
+def create_adjective_lookup():
+    adjective_lookup = {}
+
+    with open(wordlist_path, "r") as input:
+        for line in input:
+            cells = line.strip("\n").split("\t")
+
+            lemma = cells[1]
+            types = cells[3]
+            fullform = cells[4]
+
+            if "adj" in types:
+                adjective_lookup[fullform] = lemma
+
+    return adjective_lookup
+
+
+def load_category_mappings():
+    with open(categories_path, "r") as file:
+        categories = json.load(file)
+    return categories
+
+
+def load_listings():
+    category_mappings = load_category_mappings()
+
+    with open(listings_path, "r") as file:
+        listings = json.load(file)
+
+    for listing in listings:
+        listing["category"] = category_mappings.get(str(listing["category"]))
+
+    return listings
+
+
+def write_to_file(datapoint):
+    file = open(result_path, "w")
+    file.write(json.dumps(datapoint, indent=2, ensure_ascii=False))
+
+
+def find_adjectives(listing: list[object], lookup: dict[str, str]):
+    description = listing["description"]
+
+    found = []
+
+    for fullform in lookup.values():
+        check = rf"\b{fullform}\b"
+        if re.search(check, description, re.IGNORECASE):
+            found.append(lookup[fullform])
+
+    return found
+
+
+def count(words: list[str]) -> object:
+    result = {}
+
+    for word in words:
+        result[word] = result.get(word, 0) + 1
+
+    return result
+
+
+def analyze(listings, lookup):
+    category_adjectives = {}
+    for listing in listings:
+        adjectives = find_adjectives(listing, lookup)
+        combined = category_adjectives.get(listing["category"], []) + adjectives
+        category_adjectives[listing["category"]] = combined
+
+    category_adjectives_counted = {}
+    for category in category_adjectives:
+        adjectives_counted = count(category_adjectives[category])
+        category_adjectives_counted[category] = adjectives_counted
+
+    return category_adjectives_counted
+
+
+if __name__ == "__main__":
+    lookup = create_adjective_lookup()
+    listings = [x for x in load_listings() if x.get("description")]
+
+    adjective_counts_by_category = analyze(listings, lookup)
+
+    write_to_file(adjective_counts_by_category)
